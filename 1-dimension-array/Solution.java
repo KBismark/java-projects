90e6c38d62c4c51a7da8284e9c82df2efa6bc274
KBismark <bismarkkwabenayamoah@gmail.com>
2024-09-29 22:18:42 +0000

    Add new challenge: One dimenestion array problem

---
+++ 1-dimension-array/Solution.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+	   
+        Scanner scan = new Scanner(System.in);
+        int n = scan.nextInt();
+        scan.close();
+        int[] a = new Array(n);
+
+        // Prints each sequential element in array a
+        for (int i = 0; i < a.length; i++) {
+            System.out.println(a[i]);
+        }
+    }
+}
